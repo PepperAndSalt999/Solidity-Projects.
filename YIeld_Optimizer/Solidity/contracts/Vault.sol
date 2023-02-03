55951b16ad9e49fd0c74ade1283086c79d40819d
PepperAndSalt999 <for42raphaelpicot@gmail.com>
2023-02-03 05:26:49 +0100

    added withdraw function + test

--- YIeld_Optimizer/Solidity/contracts/Vault.sol
+++ YIeld_Optimizer/Solidity/contracts/Vault.sol
@@ -6,10 +6,17 @@ contract Vault
     //fallback function, allow to recieve eth
     //fallback makes it auto executed if the caller try to call an unavailable functon
     //limited to 2300 gas
-    mapping(address => uint) public addressMoney;
+    mapping(address => uint) private addressMoney;
     receive() external payable 
     {
         addressMoney[msg.sender] += msg.value;
+    }
+
+    function withdraw() external
+    {
+        uint amount;
+        amount = addressMoney[msg.sender];
+        payable(msg.sender).transfer(amount);
     }
 
     function getter() external view returns (uint)
