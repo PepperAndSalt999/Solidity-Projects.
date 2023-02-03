7f5f7bfbe867f48fdf3f7a85d8d64d441a1d20f0
PepperAndSalt999 <for42raphaelpicot@gmail.com>
2023-02-03 02:28:56 +0100

    added comments

--- YIeld_Optimizer/Solidity/test/vault.js
+++ YIeld_Optimizer/Solidity/test/vault.js
@@ -3,10 +3,12 @@ const Vault =  artifacts.require("Vault");
 
 contract("Vault", function (accounts) {
     it("able to receive money", async function(){
-        const vault = await Vault.new();
+        const vault = await Vault.new(); //create contract abastraction
+        const amount = web3.utils.toWei("0.00001", "ether"); // change eth to wei format
 
-        const amount = web3.utils.toWei("1", "ether");
-        await vault.send(web3.utils.toWei("1", "ether")); //equivalent of web3.eth.sendTransaction
-        assert.equal(await vault.getter(), amount);
+        //force truffle to make a transaction from test account 0 to contract
+        const result = await vault.send(amount, {from: accounts[0]}); //equivalent of web3.eth.sendTransaction, it is a special method embeeded on truffle contract objects.
+        console.log(result);
+        assert.equal((await vault.getter()).toNumber() / 10**18, 0.00001); //make a call aka read to get contract balance and test getter
     });
 });
