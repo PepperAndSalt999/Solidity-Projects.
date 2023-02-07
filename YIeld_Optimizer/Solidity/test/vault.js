ca55d1f0c9d0d110ae22341c13fd22832dfaf328
PepperAndSalt999 <for42raphaelpicot@gmail.com>
2023-02-07 11:48:11 +0100

    changed const of contract instance to var

--- YIeld_Optimizer/Solidity/test/vault.js
+++ YIeld_Optimizer/Solidity/test/vault.js
@@ -3,15 +3,15 @@ const Vault =  artifacts.require("Vault");
 const amount = web3.utils.toWei("0.0001", "ether"); // change eth to wei format
 let strategy_name = "test";
 let gas_fees;
+var vault;
 
-contract("Vault", function (accounts) {
+contract("Vault", function(accounts){
     it("set account, strategy and money", async function(){
-        const vault = await Vault.deployed();
+        vault = await Vault.deployed();
         await vault.setter(strategy_name, {from: accounts[0], value: amount * 2});
         assert.equal((await vault.accounts(accounts[0])).strategy_name, strategy_name);
     });
     it("send more money", async function(){ 
-        const vault = await Vault.deployed();
         let balance = await vault.accounts(accounts[0]);
         await vault.send(amount, {from: accounts[0]});
         assert.ok(balance.eth < (await vault.accounts(accounts[0])).eth);
