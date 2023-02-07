870ade74def5c9ff4ec74ebadf7e745d2f925639
PepperAndSalt999 <for42raphaelpicot@gmail.com>
2023-02-07 10:07:52 +0100

    tested Strategies.sol + interfacing from vault

--- YIeld_Optimizer/Solidity/test/strategy.js
+++ YIeld_Optimizer/Solidity/test/strategy.js
@@ -1,12 +1,12 @@
-// const Strategies = artifacts.require("Strategies");
+const Strategies = artifacts.require("Strategies");
 
-// contract("Strategy", function(accounts) {
-//     it("test strategy setter and getter", async ()=>{
-//         const strategies = await Strategies.new();
-//         const id = "test";
-//         const stratInstance =  await strategies.createStrategy(id, 9800, "test", 10, 30);
+contract("Strategy", function(accounts) {
+    it("test strategy setter and getter", async ()=>{
+        const strategies = await Strategies.new();
+        const id = "test";
+        await strategies.createStrategy(id, 9800, "test", 10, 30);
+        const stratInstance = await strategies.strategies(id);
 
-//         assert.equal(stratInstance._id, "test");
-
-//     });
-// });
+        assert.equal(stratInstance.id, "test");
+    });
+});
--- YIeld_Optimizer/Solidity/test/vault.js
+++ YIeld_Optimizer/Solidity/test/vault.js
@@ -1,34 +1,37 @@
 const Vault =  artifacts.require("Vault");
 
+const amount = web3.utils.toWei("0.0001", "ether"); // change eth to wei format
+let strategy_name = "test";
+let gas_fees;
 
-contract("Vault", function (accounts) {
-    it("able to receive money", async function(){
-        const vault = await Vault.new(); //create contract abastraction
-        const amount = web3.utils.toWei("0.00001", "ether"); // change eth to wei format
-
-        //force truffle to make a transaction from test account 0 to contract
-        const result = await vault.send(amount, {from: accounts[0]}); //equivalent of web3.eth.sendTransaction, it is a special method embeeded on truffle contract objects.
-        const result0 = await vault.getter();
-        console.log(result0);
-        assert.equal(await vault.getter(), amount); //make a call aka read to get contract balance and test getter
+contract("Vault", async (accounts) => {
+    const vault = await Vault.new();
+    it("set account, strategy and money", async function(){
+        //const vault = await Vault.new();
+        await vault.setter(strategy_name, {from: accounts[0], value: amount * 2});
+        assert.equal((await vault.accounts(accounts[0])).strategy_name, strategy_name);
     });
-    
-    it("deposit and withdraw money", async function(){
-        const vault = await Vault.new(); //create contract abastraction
-        const amount = web3.utils.toWei("0.0001", "ether"); // change eth to wei format
-        let gas_fees;
-        let options = {
-            from: accounts[0],
-            to: vault.address,
-            value: amount
-        };
-        let res = await web3.eth.getBalance(accounts[0]);
-    
-        gas_fees = (await web3.eth.estimateGas(options)) * 2;
+    it("send more money", async function(){ 
+        //const vault = await Vault.new();
         await vault.send(amount, {from: accounts[0]});
-        //from (option object )does specify the account that is gonna be used to send the transaction and pay the gaz fees.
-        
-        await vault.withdraw({from: accounts[0]});
-        //assert.ok((Number(amount) - Number(await web3.eth.getBalance(accounts[0])) - Number(gas_fees)) < 0.1); //make a call aka read to get contract balance and test getter
+        let balance = await vault.accounts(accounts[0]);
+        //assert.ok(balance.eth < (await vault.accounts(accounts[0])).eth);
     });
+
+    // it("deposit, setup strategy and withdraw money", async function(){
+
+    
+    // let options = {
+    //     from: accounts[0],
+    //     to: vault.address,
+    //     value: amount
+    // };
+    //     gas_fees = (await web3.eth.estimateGas(options)) * 2;
+    //     await vault.send(amount, {from: accounts[0]});
+    //     //from (option object )does specify the account that is gonna be used to send the transaction and pay the gaz fees.
+        
+    //     await vault.withdraw(amount, {from: accounts[0]});
+    //     assert.ok((Number(amount) - Number(await web3.eth.getBalance(accounts[0])) - Number(gas_fees)) < 0.1); //make a call aka read to get contract balance and test getter
+    // });
+
 });
