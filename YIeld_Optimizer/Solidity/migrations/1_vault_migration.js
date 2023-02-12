b02ebbfc712de5cbdc34a45e2ae793472d4943a0
PepperAndSalt999 <for42raphaelpicot@gmail.com>
2023-02-12 20:35:19 +0100

    added comments on migration vault

--- YIeld_Optimizer/Solidity/migrations/1_vault_migration.js
+++ YIeld_Optimizer/Solidity/migrations/1_vault_migration.js
@@ -1,10 +1,10 @@
 //get vault artifacts (json metadata)
 const vault = artifacts.require("Vault");
 
-
-//module.exports allow node.js to export the function
-//so that other file can access the code
-module.exports = async function(deployer) {
+//clarification on the inner workings of the modules.exports.
+//when the modules are getting imported, the require of the artifacts is going to get on the stack, and executed.
+//after that, the module function is going to be declared, but will be only executed when the module get called
+module.exports = async function(deployer, network) {
     const Vault = await vault.deployed();
     if(!Vault)
         await deployer.deploy(vault);
